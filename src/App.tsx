d07a1e9a65c586b2fa528562c6ab3b12c32624df
john-reiner <john.reiner@datadoghq.com>
2023-11-15 12:07:55 -0500

    updating to display hostname

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,12 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+
 function App() {
   const [count, setCount] = useState(0)
 
+
+  console.log(window.location.hostname)
   return (
     <>
       <div>
@@ -16,14 +19,11 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Hello from: {window.location.hostname}</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
